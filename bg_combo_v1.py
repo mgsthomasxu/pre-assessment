acf82eb91bcac378fe8d055edecc986a5119e74a
mgsthomasxu <xut2@middleton.school.nz>
2024-04-04 12:39:45 +1300

    :+1:

--- bg_combo_v1.py
+++ bg_combo_v1.py
@@ -1,8 +1,3 @@
-#the question not in middle
 import easygui
 easygui.buttonbox("What would you like to do?", choices=["Add combo", "Find combo", "Delete combo", "Output all", "Exit"], 
-                  title="MENU MAKER OPTIONS")
-
-msg = "What would you like to do?"
-easygui.buttonbox(msg=msg.center(40), choices=["Add combo", "Find combo", "Delete combo", "Output all", "Exit"], 
                   title="MENU MAKER OPTIONS")
